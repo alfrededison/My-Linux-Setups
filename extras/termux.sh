e6259f04a6e7ec1e93469d1c31becc9c8aa51ed3
Hieu Nguyen Duc <isaactesla@gmail.com>
2020-08-27 11:00:14 +0700

    customize console color

--- extras/termux.sh
+++ extras/termux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo -e "\u001b[7m Customizing theme...\u001b[0m"
+cp ./colors.properties ~/.termux/
+
 echo -e "\u001b[7m Installing dependencies...\u001b[0m"
 pkg install -y git curl zsh vim-python python cmake nodejs-lts
+
 echo -e "\u001b[7m Done.\u001b[0m"
