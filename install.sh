fe8570d35ba7639363855f107bd80276e844e59f
Nguyen Duc Hieu <isaactesla@gmail.com>
2022-11-09 09:27:03 +0700

    update fast-syntax-highlighting repo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 echo -e "\u001b[7m Cloning zsh plugins...\u001b[0m"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
 
 echo -e "\u001b[7m Installing dot files...\u001b[0m"
